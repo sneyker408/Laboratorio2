50f8bd340cbd755ce712403709e89519bcef561e
sneyker_408 <100097465+sneyker408@users.noreply.github.com>
2023-04-20 14:37:56 -0600

    Add files via upload

---
+++ parcial2.py
@@ -0,0 +1,12 @@
+# ejecutar programa while
+
+NUMEROPO = int(input("Ingrese un numero positivo"))
+
+while NUMEROPO < 0:
+    print("Error numero negrativo")
+    NUMEROPO = int(input("Ingrese un numero positivo\n"))
+
+for i in range(1,5) :
+    print(f"({NUMEROPO}/{i}) +")    
+    
+    2
